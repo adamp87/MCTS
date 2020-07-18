fix dirichlet and softmax computation

src/python/Alpha4/connect4.py
@@ -146,7 +146,7 @@ class Connect4:
         policy.shape = Connect4.dims_policy  # policy is flattened, reshape
 
         policy = np.array([policy[0, act.y, act.x] for act in actions])  # collect valid policy values
-        policy = np.exp(policy) / (np.sum(policy) + np.finfo(np.float32).eps)  # softmax
+        policy = np.exp(policy) / (np.sum(np.exp(policy)) + np.finfo(np.float32).eps)  # softmax
         return policy, value
 
     @staticmethod

src/python/Alpha4/mcts.py
@@ -32,7 +32,7 @@ class MCTS:
         """
         node = self.sub_root
         visited_nodes.append(node)  # store subroot in visited nodes
-        dirichlet_noise = np.random.dirichlet([state.ALPHA], len(node.childs))
+        dirichlet_noise = np.random.dirichlet([state.ALPHA] * len(node.childs))
         while not state.is_finished():
             if len(node.childs) == 0:  # leaf node
                 actions = state.get_actions()  # get possible actions